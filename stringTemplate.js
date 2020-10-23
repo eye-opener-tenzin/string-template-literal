c21968c623e22b3da898eaaea82bd4aa3b5f1e1c
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-22 21:11:08 -0700

    add html and js file for string template

---
+++ stringTemplate.js
@@ -0,0 +1,10 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+let roll = `Roll: You roller a ${die1} and a ${die2}. They sum to ${die1 + die2}.`
+
+document.write(roll);
+
+console.log(roll);
